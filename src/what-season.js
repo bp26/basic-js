89d1b68d0d4ad5c94a652dbaa2bc36b75495fbe6
bp26 <94074742+bp26@users.noreply.github.com>
2022-08-18 10:02:17 +0000

    feat: what season task in progress 1

--- src/what-season.js
+++ src/what-season.js
@@ -11,10 +11,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function getSeason(date) {
+ if (!date) {return 'Unable to determine the time of year!'}
+  if (Object.prototype.toString.call(date) === "[object Date]" && !isNaN(date)) {
+    const month = date.getMonth()
+    console.log(month)
+   
+      if (month < 3) {
+        return 'winter'
+      } else if (month < 6) {
+        return 'spring'
+      } else if (month < 9) {
+        return 'summer'
+      } else if (month < 12) {
+        return 'autumn'
+      } else {return 'winter'}
+
+  } else {
+    return new Error('Invalid date')
+  }
 }
+
+const obj = new Date(866, 2, 10, 12, 53, 10, 825)
+console.log(getSeason(obj))
+
+/*console.log(obj.getFullYear(), 'year')
+console.log(obj.getMonth(), 'month')
+console.log(obj.getDate(), 'day')
+console.log(obj.getHours(), 'hours')
+console.log(obj.getMinutes(), 'minutes')
+console.log(obj.getSeconds(), 'seconds')
+console.log(obj.getMilliseconds(), 'milliseconds')*/
 
 module.exports = {
   getSeason
