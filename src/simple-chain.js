367c5fa828f715bbd5f7100abe22a16b14259e1c
bp26 <aleshak260006@gmail.com>
2022-08-27 11:12:15 +0300

    feat: simple chain almost completed

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,28 +5,63 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  length: 0,
+  data: '',
+
+  setLink(value) {
+    return `( ${value} )`
+  }, 
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.length
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink(value) {
+    if (this.length !== 0) {this.data += '~~'}
+    value === undefined ? this.data += this.setLink(' ') : this.data += this.setLink(value)
+  
+
+    this.length++
+    console.log(this.data, 'Add link', `value = ${value}`)
+    return this
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  removeLink(position) {
+    if (typeof(position) !== 'number') {return new Error(`You can't remove incorrect link!`)}
+    if (position < 1 || position > this.length) {return new Error(`You can't remove incorrect link!`)}
+    
+    const dataArray = this.data.split('~~')
+    dataArray.splice(position - 1, 1)
+    this.data = dataArray.join('~~')
+    console.log(this.data, 'Remove link', `position = ${position}`)
+    return this
   },
+
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.length === 0) {return this}
+    const dataArray = this.data.split('~~')
+    this.data = dataArray.reverse().join('~~')
+    console.log(this.data, 'Reverse chain')
+    return this
   },
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let currentChain = this.data
+    console.log(currentChain)
+
+    this.data = ''
+    this.length = 0
+
+    return currentChain
   }
 };
+
 
 module.exports = {
   chainMaker
 };
+
+chainMaker.reverseChain().addLink('DEF').reverseChain().reverseChain().reverseChain().addLink(333).addLink(null).addLink(0).reverseChain().reverseChain().finishChain()
+
+
+
