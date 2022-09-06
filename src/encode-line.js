31f640c4003bc5219544f1213e3c73b521a4938a
bp26 <aleshak260006@gmail.com>
2022-09-06 15:24:08 +0300

    feat: encode line task completed

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a string, return its encoding version.
@@ -11,9 +11,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function encodeLine(str) {
-  
+  let count = 0;
+  return str
+    .split("")
+    .reduce((acc, item, index, arr) => {
+      if (index === 0) {
+        count++;
+      } else {
+        if (arr[index] === arr[index - 1]) {
+          count++;
+          if (index === arr.length - 1) {
+            acc = [...acc, (count !== 1 ? count : "") + arr[index]];
+          }
+        } else {
+          acc = [...acc, (count !== 1 ? count : "") + arr[index - 1]];
+          count = 1;
+          if (index === arr.length - 1) {
+            acc = [...acc, arr[index]];
+          }
+        }
+      }
+      return acc;
+    }, [])
+    .join("");
 }
 
 module.exports = {
-  encodeLine
+  encodeLine,
 };
