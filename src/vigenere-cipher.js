18feb3f688c425baabc325b78e6c0c3ed6ab63cd
bp26 <aleshak260006@gmail.com>
2022-09-07 12:37:23 +0300

    feat: vigenere encryption implemented

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,35 +1,109 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  alphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  constructor(boolean) {
+    this.ecryption = boolean;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    try {
+      if (message === undefined || key === undefined) {
+        throw new Error("Incorrect arguments!");
+      }
+      const messageChars = message.toUpperCase().split("");
+      const keyChars = key.toUpperCase().split("");
+      let i = 0;
+      const codeArray = messageChars.map((item) => {
+        if (this.alphabet.includes(item)) {
+          if (i === key.length) i = 0;
+          let num = item.charCodeAt(0) + keyChars[i].charCodeAt(0) - 130;
+          if (num > this.alphabet.length) {
+            item = num - this.alphabet.length;
+          } else if (num < 0) {
+            item = num + this.alphabet.length;
+          } else {
+            item = num;
+          }
+          i++;
+        }
+        return item;
+      });
+      const encryptedArray = codeArray.map((item) => {
+        if (item === " ") return item;
+        let element = String.fromCharCode(item + 65);
+        if (this.alphabet.includes(element)) {
+          item = element;
+        }
+        return item;
+      });
+
+      return this.ecryption === true
+        ? encryptedArray.join("")
+        : encryptedArray.reverse().join("");
+    } catch (error) {
+      throw error;
+    }
+  }
+  decrypt(message, key) {
+    try {
+      if (message === undefined || key === undefined) {
+        throw new Error("Incorrect arguments!");
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 }
 
+const vigenereCipheringMachine = new VigenereCipheringMachine(true);
+console.log(vigenereCipheringMachine.encrypt("attack at dawn!", "alphonse"));
+
 module.exports = {
-  VigenereCipheringMachine
+  VigenereCipheringMachine,
 };
