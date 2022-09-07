f95c18371a811758a7ea5a79d2bb07b46606e6b6
bp26 <aleshak260006@gmail.com>
2022-09-07 15:04:33 +0300

    feat: vigenere cipher task completed

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -49,7 +49,7 @@ class VigenereCipheringMachine {
     "Z",
   ];
   constructor(boolean) {
-    this.ecryption = boolean;
+    this.encryption = boolean;
   }
   encrypt(message, key) {
     try {
@@ -63,27 +63,24 @@ class VigenereCipheringMachine {
         if (this.alphabet.includes(item)) {
           if (i === key.length) i = 0;
           let num = item.charCodeAt(0) + keyChars[i].charCodeAt(0) - 130;
-          if (num > this.alphabet.length) {
-            item = num - this.alphabet.length;
+          if (num >= this.alphabet.length) {
+            item = [num - this.alphabet.length];
           } else if (num < 0) {
-            item = num + this.alphabet.length;
+            item = [num + this.alphabet.length];
           } else {
-            item = num;
+            item = [num];
           }
           i++;
         }
         return item;
       });
       const encryptedArray = codeArray.map((item) => {
-        if (item === " ") return item;
-        let element = String.fromCharCode(item + 65);
-        if (this.alphabet.includes(element)) {
-          item = element;
-        }
+        if (!Array.isArray(item)) return item;
+        item = String.fromCharCode(item[0] + 65);
         return item;
       });
 
-      return this.ecryption === true
+      return this.encryption === true || this.encryption === undefined
         ? encryptedArray.join("")
         : encryptedArray.reverse().join("");
     } catch (error) {
@@ -95,14 +92,35 @@ class VigenereCipheringMachine {
       if (message === undefined || key === undefined) {
         throw new Error("Incorrect arguments!");
       }
+      const messageChars = message.toUpperCase().split("");
+      const keyChars = key.toUpperCase().split("");
+      let i = 0;
+      const codeArray = messageChars.map((item) => {
+        if (this.alphabet.includes(item)) {
+          if (i === key.length) i = 0;
+          let num = item.charCodeAt(0) - keyChars[i].charCodeAt(0);
+          if (num < 0) {
+            item = [num + this.alphabet.length];
+          } else {
+            item = [num];
+          }
+          i++;
+        }
+        return item;
+      });
+      const decryptedArray = codeArray.map((item) => {
+        if (!Array.isArray(item)) return item;
+        item = String.fromCharCode(item[0] + 65);
+        return item;
+      });
+      return this.encryption === true || this.encryption === undefined
+        ? decryptedArray.join("")
+        : decryptedArray.reverse().join("");
     } catch (error) {
       throw error;
     }
   }
 }
-
-const vigenereCipheringMachine = new VigenereCipheringMachine(true);
-console.log(vigenereCipheringMachine.encrypt("attack at dawn!", "alphonse"));
 
 module.exports = {
   VigenereCipheringMachine,
