bf5e81eba6a233cac5f60ed9888aa267ef2e0f37
bp26 <aleshak260006@gmail.com>
2022-09-08 11:11:11 +0300

    feat: what season task almost completed

--- src/what-season.js
+++ src/what-season.js
@@ -1,48 +1,48 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
- function getSeason(date) {
- if (!date) {return 'Unable to determine the time of year!'}
-  if (Object.prototype.toString.call(date) === "[object Date]" && !isNaN(date)) {
-    const month = date.getMonth()
-    console.log(month)
-   
-      if (month < 3) {
-        return 'winter'
-      } else if (month < 6) {
-        return 'spring'
-      } else if (month < 9) {
-        return 'summer'
-      } else if (month < 12) {
-        return 'autumn'
-      } else {return 'winter'}
+function getSeason(date) {
+  try {
+    if (!date) {
+      return "Unable to determine the time of year!";
+    }
+    if (
+      Object.prototype.toString.call(date) !== "[object Date]" ||
+      isNaN(date)
+    ) {
+      throw new Error("Invalid date!");
+    }
 
-  } else {
-    return new Error('Invalid date')
+    const month = date.getMonth();
+    if (month < 2) {
+      return "winter";
+    } else if (month < 5) {
+      return "spring";
+    } else if (month < 8) {
+      return "summer";
+    } else if (month < 11) {
+      return "autumn";
+    } else {
+      return "winter";
+    }
+  } catch (error) {
+    throw error;
   }
 }
 
-const obj = new Date(866, 2, 10, 12, 53, 10, 825)
-console.log(getSeason(obj))
-
-/*console.log(obj.getFullYear(), 'year')
-console.log(obj.getMonth(), 'month')
-console.log(obj.getDate(), 'day')
-console.log(obj.getHours(), 'hours')
-console.log(obj.getMinutes(), 'minutes')
-console.log(obj.getSeconds(), 'seconds')
-console.log(obj.getMilliseconds(), 'milliseconds')*/
+const obj = new Date(866, 2, 10, 12, 53, 10, 825);
+console.log(getSeason(obj));
 
 module.exports = {
-  getSeason
+  getSeason,
 };
