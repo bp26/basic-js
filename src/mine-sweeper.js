75e9e41c4276c36db76303df597bdaf6ffbe133a
bp26 <aleshak260006@gmail.com>
2022-09-12 14:08:13 +0300

    feat: minesweeper task completed

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  return matrix.map((arr, arrIndex, arrArray) => {
+    return arr.map((num, numIndex, numArray) => {
+      let count = 0;
+
+      //upper array
+      if (arrIndex !== 0) {
+        if (arrArray[arrIndex - 1][numIndex] === true) count++;
+
+        if (numIndex !== 0) {
+          if (arrArray[arrIndex - 1][numIndex - 1] === true) count++;
+        }
+
+        if (numIndex !== numArray.length - 1) {
+          if (arrArray[arrIndex - 1][numIndex + 1] === true) count++;
+        }
+      }
+
+      //lower array
+      if (arrIndex !== arrArray.length - 1) {
+        if (arrArray[arrIndex + 1][numIndex] === true) count++;
+
+        if (numIndex !== 0) {
+          if (arrArray[arrIndex + 1][numIndex - 1] === true) count++;
+        }
+
+        if (numIndex !== numArray.length - 1) {
+          if (arrArray[arrIndex + 1][numIndex + 1] === true) count++;
+        }
+      }
+
+      //same array
+      if (numIndex !== 0) {
+        if (numArray[numIndex - 1] === true) count++;
+      }
+      if (numIndex !== numArray.length - 1) {
+        if (numArray[numIndex + 1] === true) count++;
+      }
+
+      num = count;
+      return count;
+    });
+  });
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
